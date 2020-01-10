94253d0a919352290bca0cbd8f35af8f2be929e8
amiamitswe <amiamitswe@gmail.com>
2020-01-10 21:36:09 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".menu__user").slick({
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    centerMode: false
+  });
+});
